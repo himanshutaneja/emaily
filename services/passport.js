d0cc992bee9262e6637d2e3153ba1467901d5359
Himanshu Taneja <himanshu.taneja@revenue.nsw.gov.au>
2023-11-10 16:44:56 +1100

    Google OAuth implementation

---
+++ services/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const { googleClientID, googleClientSecret } = require("../config/keys");
+const OAuth2Strategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+passport.use(
+  new OAuth2Strategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = new User({ googleId: profile.id });
+        const newUser = await user.save();
+        done(null, newUser);
+      }
+    }
+  )
+);
